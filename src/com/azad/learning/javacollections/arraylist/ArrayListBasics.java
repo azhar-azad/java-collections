5945c9ee9e8bbd396008cef889644fb2281e5c3c
azhar-azad <azhar.u.azad@gmail.com>
2020-10-14 18:54:54 +0600

    ArrayListBasics re-structured

--- src/com/azad/learning/javacollections/arraylist/ArrayListBasics.java
+++ src/com/azad/learning/javacollections/arraylist/ArrayListBasics.java
@@ -1,11 +1,21 @@
 package com.azad.learning.javacollections.arraylist;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class ArrayListBasics {
 
 	public static void main(String[] args) {
 
+		basics();
+		init();
+	}
+
+	public static void basics() {
+		
+		System.out.println("\n\nbasics\n======\n");
+		
 		// Declaration
 		ArrayList<String> strList = new ArrayList<>();
 		
@@ -42,27 +52,57 @@ public class ArrayListBasics {
 			System.out.println(str);
 		}
 		System.out.println();
+		
+		/*
+		OUTPUT:
+
+		After adding:
+		Shawon
+		Akib
+		Tipu
+		Ripon
+
+		After adding at given index:
+		Azad
+		Shawon
+		Akib
+		Tipu
+		Ripon
+
+		After removing:
+		Akib
+		Tipu
+		Ripon
+		 */
 	}
+	
+	public static void init() {
+		
+		System.out.println("\n\ninit\n====\n");
+		
+		// Initialization using Arrays.asList
+        String[] strArray = {"Tipu", "Shawon", "Akib", "Ripon"};
+        ArrayList<String> strList1 = new ArrayList<>(Arrays.asList(strArray));
+        System.out.println("strList1: " + strList1);
 
-	/*
-	OUTPUT:
+        // Anonymous inner class method
+        ArrayList<String> strList2 = new ArrayList<String>() {{
+            add("Tipu");
+            add("Shawon");
+            add("Akib");
+        }};
+        System.out.println("strList2: " + strList2);
 
-	After adding:
-	Shawon
-	Akib
-	Tipu
-	Ripon
+        // Use Collections.nCopies method
+        ArrayList<Integer> intList = new ArrayList<>(Collections.nCopies(10, 1));
+        System.out.println("intList: " + intList);
+        
+        /*
+        OUTPUT:
 
-	After adding at given index:
-	Azad
-	Shawon
-	Akib
-	Tipu
-	Ripon
-
-	After removing:
-	Akib
-	Tipu
-	Ripon
-	 */
+        strList1: [Tipu, Shawon, Akib, Ripon]
+        strList2: [Tipu, Shawon, Akib]
+        intList: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+         */
+	}
 }
--- src/com/azad/learning/javacollections/arraylist/ArrayListInit.java
+++ src/com/azad/learning/javacollections/arraylist/ArrayListInit.java
@@ -8,29 +8,8 @@ public class ArrayListInit {
 
     public static void main(String[] args) {
 
-        // Initialization using Arrays.asList
-        String[] strArray = {"Tipu", "Shawon", "Akib", "Ripon"};
-        ArrayList<String> strList1 = new ArrayList<>(Arrays.asList(strArray));
-        System.out.println("strList1: " + strList1);
-
-        // Anonymous inner class method
-        ArrayList<String> strList2 = new ArrayList<String>() {{
-            add("Tipu");
-            add("Shawon");
-            add("Akib");
-        }};
-        System.out.println("strList2: " + strList2);
-
-        // Use Collections.nCopies method
-        ArrayList<Integer> intList = new ArrayList<>(Collections.nCopies(10, 1));
-        System.out.println("intList: " + intList);
+        
     }
 
-    /*
-    OUTPUT:
-
-    strList1: [Tipu, Shawon, Akib, Ripon]
-    strList2: [Tipu, Shawon, Akib]
-    intList: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-     */
+    
 }
